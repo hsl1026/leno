ddae6153e34436982cbafbb9bd20155b0c59dd9a
long <2936875129@qq.com>
2023-01-06 10:12:12 +0800

    fix bug

--- api/lenoApi.js
+++ api/lenoApi.js
@@ -1,17 +1,37 @@
-export const text = "http://localhost:3001/newData";
+export const text = "http://localhost:3001/leno/newData";
 
-export const textNewsBox = "http://localhost:3001/newsBoxData";
+export const textNewsBox = "http://localhost:3001/leno/newsBoxData";
 
-export const textGetNewsTitle = "http://localhost:3001/newsTitle";
+export const textGetNewsTitle = "http://localhost:3001/leno/newsTitle";
 
-export const textDeleteNew = "http://localhost:3001/deleteNew";
+export const textDeleteNew = "http://localhost:3001/leno/deleteNew";
 
-export const textGetNewsContent = "http://localhost:3001/newsContent";
+export const textGetNewsContent = "http://localhost:3001/leno/newsContent";
 
-export const textGetNewsId = "http://localhost:3001/newsId";
+export const textGetNewsId = "http://localhost:3001/leno/newsId";
 
-export const textSaveImg = "http://localhost:3001/saveImg";
+export const textSaveImg = "http://localhost:3001/leno/saveImg";
 
-export const textdeleteImgsFile = "http://localhost:3001/deleteImgsFile";
+export const textdeleteImgsFile = "http://localhost:3001/leno/deleteImgsFile";
 
-export const textSendEmail = "http://localhost:3001/sendEmail";
+export const textSendEmail = "http://localhost:3001/leno/sendEmail";
+
+
+// server
+// export const text = "https://server.myth.art/leno/newData";
+
+// export const textNewsBox = "https://server.myth.art/leno/newsBoxData";
+
+// export const textGetNewsTitle = "https://server.myth.art/leno/newsTitle";
+
+// export const textDeleteNew = "https://server.myth.art/leno/deleteNew";
+
+// export const textGetNewsContent = "https://server.myth.art/leno/newsContent";
+
+// export const textGetNewsId = "https://server.myth.art/leno/newsId";
+
+// export const textSaveImg = "https://server.myth.art/leno/saveImg";
+
+// export const textdeleteImgsFile = "https://server.myth.art/leno/deleteImgsFile";
+
+// export const textSendEmail = "https://server.myth.art/leno/sendEmail";
